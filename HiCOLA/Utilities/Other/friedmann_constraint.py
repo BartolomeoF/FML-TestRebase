23b81a9a51e3c50fbe1e412b5ec41bd4d26c37f6
Ashim Sen Gupta <ashim9513@gmail.com>
2023-06-14 21:07:31 +0100

    meeting changes | friedmann_constraint.py plot to be investigated

--- HiCOLA/Utilities/Other/friedmann_constraint.py
+++ HiCOLA/Utilities/Other/friedmann_constraint.py
@@ -33,16 +33,17 @@ G4 = 0.5
 fried_RHS = eb.fried_closure(G3, G4,  K, M_pG4=M_pG4_test, M_KG4=M_KG4_test, M_G3s=M_G3s_test, M_sG4=M_sG4_test, M_G3G4=M_G3G4_test, M_Ks=M_Ks_test)
 fried_RHS_lambda = sym.lambdify([E,phiprime,omegal,omegam,omegar,k1,k2,g31,g32],fried_RHS)
 
+#[-0.397440209844333, -0.13916304083570108, -3.5804999320100737, 1.8796838411183758]
 
 phiprime_arr = np.arange(-1,1,0.01)
-E0 = np.ones(len(phiprime_arr))
-omega_l = np.zeros(len(phiprime_arr))
-omega_r = omega_l
-omega_m = np.ones(len(phiprime_arr))*0.3
-k1_arr  = np.ones(len(phiprime_arr))*-0.11442716734931437
-k2_arr = np.ones(len(phiprime_arr))*-0.29293354841424546
-g31_arr = np.ones(len(phiprime_arr))*-1.9452618449383468
-g32_arr = np.ones(len(phiprime_arr))*1.0405243750964335
+E0 = np.ones(len(phiprime_arr))*1.5612679388109132
+omega_l = np.ones(len(phiprime_arr))*0.3783143642426145
+omega_r = np.ones(len(phiprime_arr))*8.016144815327033e-05
+omega_m = np.ones(len(phiprime_arr))*0.2797409788825341#0.3
+k1_arr  = np.ones(len(phiprime_arr))*-0.397440209844333#-0.11442716734931437
+k2_arr = np.ones(len(phiprime_arr))*-0.13916304083570108#-0.29293354841424546
+g31_arr = np.ones(len(phiprime_arr))*-3.5804999320100737#-1.9452618449383468
+g32_arr = np.ones(len(phiprime_arr))*1.8796838411183758#1.0405243750964335
 
 fried_plot = fried_RHS_lambda(E0,phiprime_arr,omega_l, omega_m, omega_r, k1_arr, k2_arr, g31_arr, g32_arr)
 
@@ -65,15 +66,15 @@ for key in coeffdict:
     
 
     
-E_arr = np.arange(1,100,1)
-phiprime_arr = np.ones(len(E_arr))
-Omega_m_arr = np.ones(len(E_arr))*0.3
-Omega_l_arr = np.zeros(len(E_arr))
-Omega_r_arr = Omega_l_arr
+# E_arr = np.arange(1,100,1)
+# phiprime_arr = np.ones(len(E_arr))
+# Omega_m_arr = np.ones(len(E_arr))*0.3
+# Omega_l_arr = np.zeros(len(E_arr))
+# Omega_r_arr = Omega_l_arr
 
-k1_arr  = np.ones(len(phiprime_arr))*-0.11442716734931437
-k2_arr = np.ones(len(phiprime_arr))*-0.29293354841424546
-g31_arr = np.ones(len(phiprime_arr))*-1.9452618449383468
-g32_arr = np.ones(len(phiprime_arr))*1.0405243750964335
+# k1_arr  = np.ones(len(phiprime_arr))*-0.11442716734931437
+# k2_arr = np.ones(len(phiprime_arr))*-0.29293354841424546
+# g31_arr = np.ones(len(phiprime_arr))*-1.9452618449383468
+# g32_arr = np.ones(len(phiprime_arr))*1.0405243750964335
 
-cubic_arr = coefflambdict[phiprime**3.0](E_arr, phiprime_arr, Omega_l_arr, Omega_m_arr, Omega_r_arr, k1_arr, k2_arr, g31_arr, g32_arr)
+# cubic_arr = coefflambdict[phiprime**3.0](E_arr, phiprime_arr, Omega_l_arr, Omega_m_arr, Omega_r_arr, k1_arr, k2_arr, g31_arr, g32_arr)
