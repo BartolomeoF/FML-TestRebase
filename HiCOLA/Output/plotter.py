70f18f2ca00b60ae39b067fadfe35ebe7d0074aa
duncan <db2g21@soton.ac.uk>
2024-07-11 16:37:47 +0100

    Adjusted plots

--- HiCOLA/Output/plotter.py
+++ HiCOLA/Output/plotter.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+a_LCDM = E_LCDM = EprimeE_LCDM = Omega_m_LCDM = Omega_r_LCDM = Omega_l_LCDM = 1
+#a_LCDM, E_LCDM, EprimeE_LCDM, Omega_m_LCDM, Omega_r_LCDM, Omega_l_LCDM = np.loadtxt('LCDM.txt', unpack=True)
+
 filename ='WMAP_model_1_expansion.txt' #'{cosmology_name}_model_1_expansion.txt'
 
 a_list, E_list, phi_list, phiprime_list, Omega_m_list, Omega_r_list, Omega_lambda_list, Omega_phi_list = [], [], [], [], [], [], [], []
@@ -36,7 +39,7 @@ plt.show()
 
 plt.figure(dpi=300)
 for E in E_list:
-    plt.plot(z, E, label=r'$f_{\phi} = 1.0$')
+    plt.plot(z, E/E_LCDM, label=r'$f_{\phi} = 1.0$')
 #plt.legend(fontsize='large')
 plt.xlabel('z')
 plt.ylabel('E')
@@ -46,7 +49,7 @@ plt.show()
 
 plt.figure(dpi=300)
 for Omega_m in Omega_m_list:
-    plt.plot(z, Omega_m, label=r'$f_{\phi} = 1.0$')
+    plt.plot(z, Omega_m/Omega_m_LCDM, label=r'$f_{\phi} = 1.0$')
 #plt.legend(fontsize='large')
 plt.xlabel('z')
 plt.ylabel(r'$\Omega_m$')
@@ -55,7 +58,7 @@ plt.show()
 
 plt.figure(dpi=300)
 for Omega_r in Omega_r_list:    
-    plt.plot(z, Omega_r, label=r'$f_{\phi} = 1.0$')
+    plt.plot(z, Omega_r/Omega_r_LCDM, label=r'$f_{\phi} = 1.0$')
 #plt.legend(fontsize='large')
 plt.xlabel('z')
 plt.ylabel(r'$\Omega_r$')
@@ -69,19 +72,12 @@ for Omega_phi in Omega_phi_list:
 plt.xlabel('z')
 plt.ylabel(r'$\Omega_{\phi}$')
 plt.xscale('log')
+plt.ylim(bottom=-5, top=2.5)
 plt.show()
-
-# plt.figure(dpi=300)
-# plt.plot(z, omega_phi_cl, label=r'$f_{\phi} = 1.0$')
-# #plt.legend(fontsize='large')
-# plt.xlabel('z')
-# plt.ylabel('Omega_phi_closure')
-# plt.xscale('log')
-# plt.show()
 
 plt.figure(dpi=300)
 for Omega_lambda in Omega_lambda_list:
-    plt.plot(z, Omega_lambda, label=r'$f_{\phi} = 1.0$')
+    plt.plot(z, Omega_lambda/Omega_l_LCDM, label=r'$f_{\phi} = 1.0$')
 #plt.legend(fontsize='large')
 plt.xlabel('z')
 plt.ylabel(r'$\Omega_{\Lambda}$')
